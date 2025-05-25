2b98a3b091a24f6b63affa52bd7b84c58c3952a3
Chen Peng <chen56@msn.com>
2025-05-25 09:41:37 +0800

    注释掉：#set -o errtrace  # -E trap inherited in sub script                       # don't use it : print too many error , too complicated,

--- sha.bash
+++ sha.bash
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-set -o errtrace  # -E trap inherited in sub script
+#set -o errtrace  # -E trap inherited in sub script
+                  # don't use it : print too many error , too complicated,
 set -o errexit   # -e
 set -o functrace # -T If set, any trap on DEBUG and RETURN are inherited by shell functions
 set -o pipefail  # default pipeline status==last command status, If set, status=any command fail
